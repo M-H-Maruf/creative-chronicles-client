90cc2fb19c45301619e11b8b1ef074ca0cb972ef
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-11-04 22:09:16 +0600

    favicon and title added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from './components/shared/Navbar';
 
 
 const App = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
   );
