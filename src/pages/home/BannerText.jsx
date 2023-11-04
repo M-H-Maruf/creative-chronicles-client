994015ce9bcab9760bbcf0bd274e9b63afa9be11
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-11-05 03:05:30 +0600

    banner text added

---
+++ src/pages/home/BannerText.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
+const BannerText = () => {
+  return (
+    <div
+      data-aos="fade-right"
+      className="absolute flex flex-col justify-center items-center"
+    >
+      <h1 className="text-white text-center text-4xl opacity-90 font-teko tracking-tight block">
+      DELVE INTO A TAPESTRY OF TOPICS INCLUDING
+      </h1>
+      <hr />
+
+      <p
+        data-aos="fade-up-left"
+        className="w-80 text-white opacity-60 mt-4 font-medium text-center"
+      >
+       Welcome to Creative Chronicles, your sanctuary for boundless creativity. Here, we embrace artists, writers, designers, and visionaries. Dive into a world of art, literature, and innovation. Through insightful articles and a supportive community, we nurture your creative journey. Join us in celebrating the power of imagination and self-expression. Let's embark on this enriching voyage together!
+      </p>
+    </div>
+  );
+};
+
+export default BannerText;
